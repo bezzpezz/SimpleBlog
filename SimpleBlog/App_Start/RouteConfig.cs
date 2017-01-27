140b7082758140d6461f9442cad4efa17778de1b
Kepler186f\beren <bezz.pezz@hotmail.com>
2017-01-27 13:17:48 +1000

    Added basic routes and controllers and admin area

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SimpleBlog.Controllers;
 
 namespace SimpleBlog
 {
@@ -13,11 +14,35 @@ namespace SimpleBlog
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //not working??
+            //routes.MapRoute("Home", "", new { contoller = "Posts", action = "Index" });
+
+            //this stores a string array of possibly confolicting PostCotrollers in differing areas to use in the routing below
+            //when passed in it makes sure the routes use the correct namespace and therefore correct controller - action 
+            var namespaces = new[] { typeof(PostsController).Namespace };
+
+            //Login Route
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "Login",
+                url: "login",
+                defaults: new { controller = "Auth", action = "Login" },
+                namespaces: namespaces
             );
+
+            //Home Route
+            routes.MapRoute(
+                name: "Home",
+                url: "",
+                defaults: new { controller = "Posts", action = "Index"},
+                namespaces: namespaces
+            );
+
+            //Default
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
